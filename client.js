332ccc679af535f3950da1217313dc00d5bc7c19
OUARRICH <warichsaid@gmail.com>
2020-08-09 12:29:56 +0200

    module3

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const url = require('url')
 const bodyParser = require("body-parser")
 const axios = require("axios").default
 const { randomString, timeout } = require("./utils")
@@ -26,6 +27,51 @@ app.use(bodyParser.urlencoded({ extended: true }))
 /*
 Your code here
 */
+app.get('/authorize', (req, res) => {
+	state = randomString();
+	const redirectUri = url.parse(config.authorizationEndpoint);
+	redirectUri.query = {
+		response_type: "code",
+		client_id: config.clientId,
+		redirect_uri: config.redirectUri,
+		scope :"permission:name permission:date_of_birth",
+		state
+	};
+	res.redirect(url.format(redirectUri));
+});
+
+app.get('/callback', (req, res) => {
+	if(req.query.state !== state){
+		res.status(403).end();
+		return;
+	}
+	axios({
+		method: 'POST',
+		url: config.tokenEndpoint,
+		auth: {
+		username: config.clientId,
+		password: config.clientSecret
+		},
+		data: {
+		code: req.query.code,
+		} 
+	}).then(response => {
+		return axios({
+			method: 'GET',
+			url: config.userInfoEndpoint,
+			headers: {
+				authorization: `bearer ${response.data.access_token}`
+			}	
+		}).then(response => {
+			res.render('welcome', {
+				user: response.data
+			});
+		}).catch(e => {
+			console.log(e);
+			res.status(500).end('something went wrong')
+		})
+	});
+})
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
